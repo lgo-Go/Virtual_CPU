e9a3b7effbcbaa3d64a11024f3752ae49cf8478e
Игорь <pupokbomzha@yandex.ru>
2018-06-02 00:47:08 +0300

    + чтение и сохранение команды

--- Virtual_CPU.c
+++ Virtual_CPU.c
@@ -4,26 +4,30 @@
 int main(void)
 {
 	int i = 0;
-	char ch;
-	char comand[30];
+	int size_comand = 1;
+	char comand[size_comand];
 	int memory[256];
 	int reg_a, reg_b, reg_c, reg_d,
 		carry_flag, zero_flag,
 		*stack_pointer, *instruction_pointer;
 	FILE *pf;
 
+
 	if((pf = fopen("assembler_code.txt", "r")) == NULL)
 	{
 		fprintf(stderr, "file open error");
 		exit(EXIT_FAILURE);
 	}
+
 	stack_pointer = & memory[231];
 	instruction_pointer = memory;
 
-	while((ch = getc(pf)) != '\n')
+	while((comand[i] = getc(pf)) != ' ')
 	{
-		comand[i] == ch;
+		if(comand[i] == EOF)
+			break;
 		++i;
+		++size_comand;
 	}
 
 	printf("%s\n", comand);
